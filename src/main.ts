18fba837080c1f605f4f5e0e340984f030a1cb56
pavlo606 <pavlyk.dykyy@gmail.com>
2025-10-28 17:14:14 +0200

    Run server on 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "http://192.168.0.111:5173"],
     credentials: true,
   })
   app.use(cookieParser());
@@ -33,6 +33,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new PrismaExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
